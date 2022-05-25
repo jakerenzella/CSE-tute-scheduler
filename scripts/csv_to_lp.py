f402a6556d9e8bc173976b6a6750ab8da5c98d13
Jake Renzella <j@kerenzella.com>
2022-05-26 02:08:07 +1000

    new: add headerdoc

--- scripts/csv_to_lp.py
+++ scripts/csv_to_lp.py
@@ -4,12 +4,17 @@
 import csv
 import pathlib
 
-WORKING_DIR = pathlib.Path(__file__).parent.absolute().parent.absolute()
+WORKING_DIR = pathlib.Path(__file__).parent.parent.absolute()
 DATA_DIR = WORKING_DIR / 'sample_data'
 OUTPUT_DIR = WORKING_DIR / 'data'
 
 
-def load_timetable():
+def load_timetable() -> list:
+    """Loads the timetable CSV into a dictionary
+
+    Returns:
+        List: A dictionary containing the CSV data
+    """
     results = []
     with open(pathlib.Path(DATA_DIR / 'timetable.csv'), encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -20,21 +25,41 @@ def load_timetable():
     return results
 
 
-def timetable_entry_to_lp(timetable_entry):
+def save_lp(lp_list: list):
+    """Generates the .lp file from the timetable CSV, and saves it to a file
+    """
+    OUTPUT_DIR.mkdir(exist_ok=True)
+    with open(pathlib.Path(OUTPUT_DIR / 'timetable.lp'), 'w', encoding='utf-8') as lpfile:
+        lpfile.write('\n'.join(lp_list))
+
+
+def timetable_entry_to_lp(timetable_entry: dict) -> str:
+    """Converts a single timetable entry to a .lp format
+
+    Args:
+        timetable_entry (dict): The timetable entry to convert
+
+    Returns:
+        str: the lp format of the timetable entry
+    """
     keys_to_extract = ['Class Desc', 'In Person',
                        'Room', 'Day', 'Time', 'Code']
 
     return f"class({', '.join((timetable_entry[key].lower()) for key in keys_to_extract)})."
 
 
-def timetable_dict_to_lp(timetable_dict):
-    return [timetable_entry_to_lp(x) for x in timetable_dict]
+def timetable_dict_to_lp(timetable: list) -> list:
+    """Iterates through the list of timetable entries and converts them to .lp format
+
+    Args:
+        timetable (list): the list of timetable entries
+
+    Returns:
+        list: the list of .lp formatted timetable entries
+    """
+    return [timetable_entry_to_lp(x) for x in timetable]
 
 
-def timetable_lp_to_file():
-    OUTPUT_DIR.mkdir(exist_ok=True)
-    with open(pathlib.Path(OUTPUT_DIR / 'timetable.lp'), 'w', encoding='utf-8') as lpfile:
-        lpfile.write('\n'.join(timetable_dict_to_lp(load_timetable())))
-
-
-timetable_lp_to_file()
+if __name__ == '__main__':
+    lp_data = timetable_dict_to_lp(load_timetable())
+    save_lp(lp_data)
