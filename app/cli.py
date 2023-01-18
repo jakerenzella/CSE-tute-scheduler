863a7354a99ff15620ca4231fbbe48890f88f824
jakerenzella <j@kerenzella.com>
2023-01-18 22:27:18 +1100

    docs: update typer hints

--- app/cli.py
+++ app/cli.py
@@ -4,11 +4,10 @@ import typer
 from allocate import run_allocate
 
 
-def main(course_code: str = typer.Argument(..., help="The name of the user to greet"), preferences_input_file: str = typer.Argument(..., help="The name of the user to greet"), timetable_input_file: str = typer.Argument(..., help="The name of the user to greet")):
+def main(course_code: str = typer.Argument(..., help="The course code (comp1531"), preferences_input_file: str = typer.Argument(..., help="The path to the preferences csv file"), timetable_input_file: str = typer.Argument(..., help="The path to the timetable csv file")):
     """Runs the allocate script"""
 
-    course_code = course_code.lower()
-    run_allocate(course_code, preferences_input_file, timetable_input_file)
+    run_allocate(course_code.lower(), preferences_input_file, timetable_input_file)
 
 
 if __name__ == "__main__":
